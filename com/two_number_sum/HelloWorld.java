be0dcc7d259033f5bc2b067c44ab19b511c00e38
alternateaccount123 <brandon_i_a@hotmail.com>
2022-11-12 18:02:12 -0800

    traverse

--- com/two_number_sum/HelloWorld.java
+++ com/two_number_sum/HelloWorld.java
@@ -10,16 +10,24 @@ class HelloWorld {
 		System.out.println("Hello world!");
 
 		int [] array = {3,5,-4,8,11,1,-1,6};
-
 		int targetSum = 10; 
-
 		int cur_sum = 0; 
 
 		for (int i = 0; i < array.length ; i++){
 			for (int j = 0; j < array.length ; j++){
 				
-				//problem: elements cannot be the same i and j cannot be equal 
-				cur_sum = array[i] + array[j];
+				if (i == j){
+					//ignore current check 
+				}else{
+
+					if (array[i] + array[j] == targetSum){
+						System.out.println("i " + array[i] + "j " + array[j]);
+						//put into array
+					}
+
+				}
+				
+				
 				
 			}
 		}
