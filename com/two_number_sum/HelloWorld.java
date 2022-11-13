ea9a82cdeb5101fa266430cbcd349f7bce1a7f23
alternateaccount123 <brandon_i_a@hotmail.com>
2022-11-12 19:28:08 -0800

    store the right values

--- com/two_number_sum/HelloWorld.java
+++ com/two_number_sum/HelloWorld.java
@@ -1,4 +1,7 @@
-//objective: template for java file
+import java.util.ArrayList;
+
+//reason why I not use the loop pattern : this is a straight translation from paper. 
+//https://www.algoexpert.io/questions/two-number-sum
 //import java.util.*;
 
 class HelloWorld {
@@ -10,8 +13,10 @@ class HelloWorld {
 		System.out.println("Hello world!");
 
 		int [] array = {3,5,-4,8,11,1,-1,6};
+		//int [] array = {1,2,3,7};
 		int targetSum = 10; 
-		int cur_sum = 0; 
+		int cur_sum = 0;
+		ArrayList <Integer> ans_arrl = new ArrayList<Integer>();
 
 		for (int i = 0; i < array.length ; i++){
 			for (int j = 0; j < array.length ; j++){
@@ -21,8 +26,14 @@ class HelloWorld {
 				}else{
 
 					if (array[i] + array[j] == targetSum){
+
 						System.out.println("i " + array[i] + "j " + array[j]);
-						//put into array
+						cur_sum = array[i] + array[j];
+						//put in memory 
+						if(!ans_arrl.contains(array[i]) || !ans_arrl.contains(array[j])){ //not allow duplicates
+							ans_arrl.add(array[i]); 
+							ans_arrl.add(array[j]); 
+						}
 					}
 
 				}
@@ -31,6 +42,19 @@ class HelloWorld {
 				
 			}
 		}
+
+
+
+		//System.out.println(ans_arrl);//debug
+		int [] ans_arr = new int[ans_arrl.size()];
+		for (int i = 0 ; i < ans_arrl.size() ; i++){
+			ans_arr[i] = ans_arrl.get(i);
+		}
+
+		//debug
+		//for (int i = 0 ; i < ans_arrl.size() ; i++){  
+		//	System.out.println(ans_arr[i]);
+		//}
 
 
 	}
