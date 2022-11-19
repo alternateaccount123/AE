cb6434136847624814063cf20141feeea751a131
alternateaccount123 <brandon_i_a@hotmail.com>
2022-11-19 15:48:48 -0800

    add condition to end the loop if it needs to, true if contains

--- com/validate_subsequence/HelloWorld.java
+++ com/validate_subsequence/HelloWorld.java
@@ -6,8 +6,8 @@ class HelloWorld {
 
 	public static void main(String[] args) {
 
-
-
+		/* 
+		//to memory 
 		ArrayList <Integer> set = new ArrayList <Integer>();
 		set.add(1);
 		set.add(2);
@@ -16,11 +16,28 @@ class HelloWorld {
 		ArrayList <Integer> subset = new ArrayList <Integer>();
 		subset.add(1);
 		subset.add(4);
+		*/
+
+		ArrayList <Integer> set = new ArrayList <Integer>();
+		set.add(5);
+		set.add(1);
+		set.add(22);
+		set.add(25);
+		set.add(6);
+		set.add(-1);
+		set.add(8);
+		set.add(10);
+
+		ArrayList <Integer> subset = new ArrayList <Integer>();
+		subset.add(1);
+		subset.add(6);
+		subset.add(-1);
+		subset.add(10);
 
 
+		//traverse
 		for (int i = 0 ; i < subset.size(); i++){
 			
-			//System.out.println(set.get(i));
 			
 			int cur_subset_el_index = subset.get(i);
 
