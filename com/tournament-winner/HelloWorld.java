17426a7230a79f292b6367614f2eab83d835b91a
alternateaccount123 <brandon_i_a@hotmail.com>
2022-11-26 15:51:50 -0800

    identifying which team wins based on results

--- com/tournament-winner/HelloWorld.java
+++ com/tournament-winner/HelloWorld.java
@@ -9,7 +9,7 @@ class HelloWorld {
 		
 		//store information about the competitions
 		ArrayList <ArrayList<String>> competitions = new ArrayList<>(); 		
-		//each 1d arraylist form of [homeTeam,results]
+		//each 1d arraylist form of [homeTeam,awayteam]
 		competitions.add(new ArrayList<String>(Arrays.asList("HTML", "C#")));
 		competitions.add(new ArrayList<String>(Arrays.asList("C#", "Python")));
 		competitions.add(new ArrayList<String>(Arrays.asList("Python", "HTML")));
@@ -19,12 +19,30 @@ class HelloWorld {
 		results.add(0);results.add(0);results.add(1);
 
 
+		ArrayList <String> cur_competition ;
+		int cur_result;
+		HashMap <String,Integer> table_of_scores = new HashMap <String, Integer> ();
+		int cur_score = 0;
 
 		//traverse competitions 
 		for (int i = 0 ; i< competitions.size() ;i++){
-			System.out.println(competitions.get(i));
+
+			cur_competition = competitions.get(i);
+			cur_result = results.get(i);
+			
+			System.out.println("cur_competition" + cur_competition + "\tcur_result: " + cur_result);
+
+			if (cur_result == 0){ //awayteam won 
+				//table_of_scores.add(cur_competition.get(1), Integer(cur_score + cur_result));
+			}
+
+			if (cur_result == 1){  //hometeam won 
+				//table_of_scores.add(cur_competition.get(0), cur_score + cur_result);
+			}
+
 		}
 		
+		System.out.println(table_of_scores);
 
 
 	}
